3ada2a29c9ec158507a1defb9876a909cd06e00e
Nardo Michele <nardom@pascal.local>
2025-04-30 10:04:01 +0200

    struct nel file .h e output di cio che contiene

---
+++ es_prova/prova.h
@@ -0,0 +1,12 @@
+#ifndef PROVA_H_INCLUDED
+#define PROVA_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+struct prova{
+    int numero;
+    string ciao;
+};
+
+
+#endif // PROVA_H_INCLUDED
