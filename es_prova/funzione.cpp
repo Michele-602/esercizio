8f1e353a3f15d6b38ba764144d0befd3f18bfc6c
Marciano' Filippo <marcianof@pascal.local>
2025-04-30 10:14:00 +0200

    FIX: invertito operatore

--- es_prova/funzione.cpp
+++ es_prova/funzione.cpp
@@ -11,7 +11,7 @@ void visua(int v[], int n){
 void ordina(int v[], int n){
     for (int  i=0; i<n-1; i++){
         for (int j = i+1; j<n; j++){
-            if(v[i]>v[j])  swap(v[i], v[j]);
+            if(v[i]<v[j])  swap(v[i], v[j]);
         }
     }
 }
